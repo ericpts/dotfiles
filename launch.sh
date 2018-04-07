50ba6efef724ec680396024d319d313e4fa1a281
Eric Stavarache <ericptst@gmail.com>
2018-04-07 11:54:07 +0300

    Use ohmyzsh instead of zprezto

--- launch.sh
+++ launch.sh
@@ -2,9 +2,9 @@
 
 function install_packages() {
     echo "Installing packages..." | tee -a install.log
-    sudo apt-get update &>> install.log
+    sudo apt update
 
-    sudo apt-get install\
+    sudo apt install -y\
         ack-grep \
         bash \
         curl \
@@ -21,62 +21,78 @@ function install_packages() {
         tmux \
         wget \
         zsh \
-        &>> install.log
+        neovim \
+
 
     # Install neovim and set the proper alternatives.
-    sudo pip3 install neovim &>> install.log
-    sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 80 &>> install.log
-    sudo update-alternatives --set vim /usr/bin/nvim &>> install.log
+    sudo pip3 install neovim
+    sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 80
+    sudo update-alternatives --set vim /usr/bin/nvim
 
     # Install rust.
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path &>> install.log
+    curl https://sh.rustup.rs -sSf | sh -s -- -y --no-modify-path
 }
 
 function make_symlinks() {
     echo "Making symlinks..." | tee -a install.log
     for f in ~/dotfiles/home/.*; do
         if [ -f "$f" ]; then
-            echo "Creating symlink to $(basename $f)" &>> install.log
-            ln -sf "$f" "$HOME/$(basename "$f")" &>> install.log
+            echo "Creating symlink to $(basename $f)"
+            ln -sf "$f" "$HOME/$(basename "$f")"
         fi
     done
 }
 
 function install_plugins() {
-    echo "Installing plugins..." | tee -a install.log
+    echo "Installing plugins..."
 
-    # Install zprezto.
-    if [ -e ~/.zprezto ]; then
-        rm -rf ~/.zprezto
+    # Install ohmyzsh.
+    if [ -e ~/.oh-my-zsh ]; then
+        rm -rf ~/.oh-my-zsh
     fi
-    git clone https://github.com/sorin-ionescu/prezto.git ~/.zprezto &>> install.log
+
+
+    # Change shell to zsh
+    chsh -s $(which zsh)
+    sudo chsh -s $(which zsh)
 
     (
-    cd ~/.zprezto
-    git submodule update --init --recursive &>> install.log
+    echo "Installing ohmyzsh..."
+    ZSH=~/.oh-my-zsh
+    git clone --depth=1 https://github.com/robbyrussell/oh-my-zsh.git $ZSH
+
+    if [ -f ~/.zshrc ] || [ -f ~/.zshrc ]; then
+        echo "Backing up existing .zshrc"
+        mv ~/.zshrc ~/.zshrc_pre_ohmyzsh
+    fi
     )
+
+    (
+      cd ~/.oh-my-zsh/custom/plugins
+      # Install zsh-syntax-highlighting for ohmyzsh
+      git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
+
+      # Install zsh-autosugestions for ohmyzsh
+      git clone https://github.com/zsh-users/zsh-autosuggestions
+    )
+
 
     # Install tmux plugins.
     if [ -e ~/.tmux/plugins/tpm ]; then
         rm -rf ~/.tmux/plugins/tpm
     fi
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm &>> install.log
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
     ~/.tmux/plugins/tpm/bin/update_plugins all
 
     # Install all vim plugins in ex mode.
     vim +PlugInstall! +qall
-
-    # Change shell to zsh
-    chsh -s $(which zsh)
-    sudo chsh -s $(which zsh)
 }
 
 set -e
 
-rm -f install.log
 git pull &> /dev/null
 install_packages
-make_symlinks
 install_plugins
+make_symlinks
 
 echo "Done!"
